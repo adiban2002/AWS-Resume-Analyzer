dbe9d76b9d9c0e71ddfe47d59a6af1017583bd7d
Aditya Banerjee <adityaban666@gmail.com>
2026-03-07 14:52:25 +0530

    Frontend added

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.api import upload
 from app.api import analyze
 from app.api import history
 
 app = FastAPI(title="AWS Resume Analyzer")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(upload.router)
 app.include_router(analyze.router)
